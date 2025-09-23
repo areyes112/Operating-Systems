a135b7f159a418d2e2e42a3900f8a89bd2477f4a
Aiden Benton <herobro0624@gmail.com>
2025-09-23 11:45:45 -0500

    Minor Improvements, Code Produces An Output Now

--- Threads.py
+++ Threads.py
@@ -1,16 +1,15 @@
 class ProcessManagement:
-    def init(self, name, SID, age):
+    def __init__(self, name, SID, age):
         self.name = name
         self.SID = SID
         self.age = age
-    
-    def printing(self):
-        print(Process1.name, Process1.SID, Process1.age)
 
-if __name__ == "__name__":
-    Process1 = ('Aidan', '900000000', 21)
-    Process2 = ('Tyler','900000001', 22 )
+    def printing(self):
+        print(self.name, self.SID, self.age)
+
+if __name__ == "__main__":
+    Process1 = ProcessManagement('Aidan', '900000000', 21)
+    Process2 = ProcessManagement('Tyler', '900000001', 22)
 
     print("This is for threads")
-
-    print(Process1.printing())
+    Process1.printing()
