ae453b718434d29cc65f3801a1b78f8bbc007873
Aiden Benton <herobro0624@gmail.com>
2025-09-23 12:41:09 -0500

    Added additional functions/threads

--- Threads.py
+++ Threads.py
@@ -1,26 +1,43 @@
 import threading
 import time
 class ProcessManagement:
-    def __init__(self, name, SID, age):
+    def __init__(self, name, SID, age, delay):
         self.name = name
         self.SID = SID
         self.age = age
+        self.delay = delay
 
     def printing(self):
         print(self.name, self.SID, self.age)
 
+    def study(self):
+        for i in range(5):
+            print(f"{self.name} has studied for {i+1} hours")
+            time.sleep(self.delay)
+        print(f"{self.name} has finished studying")
+
+
 if __name__ == "__main__":
     #Variable assignment
-    Process1 = ProcessManagement('Aidan', '900000000', 21)
-    Process2 = ProcessManagement('Tyler', '900000001', 22)
+    Process1 = ProcessManagement('Aidan', '900000000', 21, 2)
+    Process2 = ProcessManagement('Tyler', '900000001', 22, 3)
     #Thread assignment
     thread1 = threading.Thread(target=Process1.printing)
     thread2 = threading.Thread(target=Process2.printing)
+    thread3 = threading.Thread(target=Process1.study)
+    thread4 = threading.Thread(target=Process2.study)
+
     #Starting threads
     thread1.start()
     thread2.start()
+    thread3.start()
+    thread4.start()
+
+
     #Joining threads
     thread1.join()
     thread2.join()
+    thread3.join()
+    thread4.join()
     #Checking if threads are completed
     print("Threading completed")
