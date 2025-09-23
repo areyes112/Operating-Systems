3b9e19741db68bb82b365f9b2e506d1cbbb84fb6
Aiden Benton <herobro0624@gmail.com>
2025-09-23 12:23:51 -0500

    Added Thread Functions

--- Threads.py
+++ Threads.py
@@ -1,3 +1,5 @@
+import threading
+import time
 class ProcessManagement:
     def __init__(self, name, SID, age):
         self.name = name
@@ -8,9 +10,17 @@ class ProcessManagement:
         print(self.name, self.SID, self.age)
 
 if __name__ == "__main__":
+    #Variable assignment
     Process1 = ProcessManagement('Aidan', '900000000', 21)
     Process2 = ProcessManagement('Tyler', '900000001', 22)
-
-    print("This is for threads")
-    Process1.printing()
-    Process2.printing()
+    #Thread assignment
+    thread1 = threading.Thread(target=Process1.printing)
+    thread2 = threading.Thread(target=Process2.printing)
+    #Starting threads
+    thread1.start()
+    thread2.start()
+    #Joining threads
+    thread1.join()
+    thread2.join()
+    #Checking if threads are completed
+    print("Threading completed")
