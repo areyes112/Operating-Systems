e61df6b43bda1bd220da3a1f37224d8aa58c2b34
Aiden Benton <herobro0624@gmail.com>
2025-10-21 15:30:17 -0500

    Updated Functions

--- StackHeap.py
+++ StackHeap.py
@@ -1,20 +1,32 @@
 from heapq import heapify, heappush, heappop   
-from collections import deque
+
+class StackMath:
+    def __init__(self,val_a,val_b,val_c):
+        self.val_a = val_a
+        self.val_b = val_b
+        self.val_c = val_c
+
+    def summation(self, n):
+        total_a = 0
+        total_b = 0
+        total_c = 0
+        for i in range(n + 1):
+            total_a += self.val_a
+            total_b += self.val_b
+            total_c += self.val_c
+        return total_a, total_b, total_c
+    
+    def factorial(self):
+        def fact(n):
+            if n == 0 or n == 1:
+                return 1
+            else:
+                return n * fact(n - 1)
+        return fact(self.val_a), fact(self.val_b), fact(self.val_c)
 
 if __name__ == "__main__":
-    # Demonstration of Stack using deque
-    stack = deque()
+
+    num1 = StackMath(3, 4, 5)
+    print("Factorials:", num1.factorial())
     
-    stack.append(1)
-    stack.append(2)
-    stack.append(3)
-    print("Stack pop:", stack.pop())  # Outputs: 3
-
-    # Demonstration of Heap using heapq
-    heap = []
-    heapify(heap)
-
-    heappush(heap, 5)
-    heappush(heap, 1)
-    heappush(heap, 3)
-    print("Heap pop:", heappop(heap))  # Outputs: 1
+    
