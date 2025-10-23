0ff0700d47c53b9c79b5fe129605b5764d0bda71
Aiden Benton <herobro0624@gmail.com>
2025-10-23 11:12:45 -0500

    Editted to better display Stacks

--- StackHeap.py
+++ StackHeap.py
@@ -6,27 +6,30 @@ class StackMath:
         self.val_b = val_b
         self.val_c = val_c
 
-    def summation(self, n):
-        total_a = 0
-        total_b = 0
-        total_c = 0
-        for i in range(n + 1):
-            total_a += self.val_a
-            total_b += self.val_b
-            total_c += self.val_c
-        return total_a, total_b, total_c
-    
-    def factorial(self):
-        def fact(n):
-            if n == 0 or n == 1:
-                return 1
-            else:
-                return n * fact(n - 1)
-        return fact(self.val_a), fact(self.val_b), fact(self.val_c)
+    def func1(self):
+        print("This is function 1 starting...")
+        print("We will see function 2 complete before function 1 ends")
+        self.func2()
+        print("Function 1 ending now.")
 
+    def func2(self):
+        print("Function 2 starting...")
+        print("We will see function 3 complete before function 2 ends")
+        self.func3()
+        print("Function 2 ending now.")
+
+    def func3(self):
+        print("This is function 3 starting...")
+        print("Function 3 ending now.")
+        return self.val_a + self.val_b + self.val_c
+    
 if __name__ == "__main__":
 
-    num1 = StackMath(3, 4, 5)
-    print("Factorials:", num1.factorial())
+    heap1 = [1,2,3]
+    heap2 = [4,5,6]
+    StackMathInstance1 = StackMath(heap1)
+    StackMathInstance2 = StackMath(heap2)
+    StackMathInstance1.func1()
+    StackMathInstance2.func1()
     
     
