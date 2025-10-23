934f0c1032806225ca529c3eb6b554d342ae12e0
Aiden Benton <herobro0624@gmail.com>
2025-10-23 11:39:12 -0500

    Improved to show Heaps better

--- StackHeap.py
+++ StackHeap.py
@@ -1,32 +1,43 @@
 class StackMath:
-    def __init__(self,val_a,val_b,val_c):
+    def __init__(self, val_a, val_b, val_c):
         self.val_a = val_a
         self.val_b = val_b
         self.val_c = val_c
 
+        self.heap_ref = [val_a, val_b, val_c]
+
+    def startFunc(self):
+        print("\n=== Demonstrating Stack (FILO) Behavior ===\n")
+        self.funcA()
+        print("\n=== Stack Unwinding Complete ===\n")
+
+        print("\n=== Demonstrating Heap (Memory Storage) Behavior ===\n")
+        self.showHeap()
+
     def funcA(self):
-        print("==========Function 1 Starting==========\n")
+        print("========== Function A Starting ==========\n")
         self.funcB()
-        print("==========Function 1 Ending==========\n")
+        print("========== Function A Ending ==========\n")
 
     def funcB(self):
-        print("----------Function 2 Starting----------\n")
+        print("---------- Function B Starting ----------\n")
         self.funcC()
-        print("----------Function 2 Ending----------\n")
+        print("---------- Function B Ending ----------\n")
 
     def funcC(self):
-        print("###########Function 3 Starting###########\n")
-        print(f"Values are: {self.val_a}, {self.val_b}, {self.val_c}\n")
-        print("###########Function 3 Ending###########\n")
+        print("########### Function C Starting ###########\n")
+        print(f"Values in local scope: {self.val_a}, {self.val_b}, {self.val_c}\n")
+        print("########### Function C Ending ###########\n")
 
-    
+    def showHeap(self):
+        print(f"Heap reference ID: {id(self.heap_ref)}")
+        print(f"Heap contents: {self.heap_ref}")
+        print("Changing heap contents...")
+        self.heap_ref[0] = 99   
+        print(f"Heap contents after modification: {self.heap_ref}")
+        print("Notice how the object itself stayed the same, but its contents changed.\n")
+
 if __name__ == "__main__":
-
-    heap1 = [1,2,3]
-
+    heap1 = [1, 2, 3]
     StackMathInstance1 = StackMath(*heap1)
-
-    StackMathInstance1.funcA()
-
-    
-    
+    StackMathInstance1.startFunc()
