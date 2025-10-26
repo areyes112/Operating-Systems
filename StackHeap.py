541c784dcc951997cc3f044f9a02a1666d29933b
Aiden Benton <herobro0624@gmail.com>
2025-10-26 12:26:25 -0500

    Finalized Code, added Snapshot at an additional point

--- StackHeap.py
+++ StackHeap.py
@@ -43,6 +43,8 @@ class StackMath: # Demonstrates stack and heap behavior
         print("========== Function C  =========/ \n")
 
     def showHeap(self): # Demonstrates heap behavior
+
+        snapshot_heap1 = tracemalloc.take_snapshot()  # take a snapshot before heap operations
         print(f"Heap reference ID: {id(self.heap_ref)}") # This calls the memory address of the heap object
         current, peak = tracemalloc.get_traced_memory() # Get current and peak memory usage
         print(f"Current memory usage: {current / 10**6}MB; Peak: {peak / 10**6}MB") # Display memory usage
@@ -55,10 +57,17 @@ class StackMath: # Demonstrates stack and heap behavior
         print(f"Heap contents after modification: {self.heap_ref}")
         print(f"Heap reference ID again: {id(self.heap_ref)}") # Shows that the memory address remains the same
         print("Notice how the object itself stayed the same, but its contents changed.\n")
+        snapshot_heap2 = tracemalloc.take_snapshot()  # take another snapshot after heap operations
+        stats_heap = snapshot_heap1.compare_to(snapshot_heap2, 'lineno')
+        stats_heap = [stat for stat in stats_heap if "StackHeap.py" in str(stat.traceback)]
+        print("=== Memory Allocation Stats During Heap Operations ===\n")
+        for stat in stats_heap[:5]:
+            print(stat)
 
 if __name__ == "__main__": # Main execution
 
     snapshot_main1 = tracemalloc.take_snapshot()  # take a snapshot before creating instances
+
     heap1 = [1, 2, 3]
     StackMathInstance1 = StackMath(*heap1)
     StackMathInstance1.startFunc()
@@ -66,6 +75,7 @@ if __name__ == "__main__": # Main execution
     heap2 = ["a", "b", "c"]
     StackMathInstance2 = StackMath(*heap2)
     StackMathInstance2.startFunc()
+
     snapshot_main2 = tracemalloc.take_snapshot()  # take another after creating second instance
     stats_main1 = snapshot_main1.compare_to(snapshot_main2, 'lineno')
     stats_main1 = [stat for stat in stats_main1 if "StackHeap.py" in str(stat.traceback)]
